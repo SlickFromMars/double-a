c89d140f60b8c68d5e9d3c6669742847a8a4a646
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2023-03-02 13:12:10 -0500

    Update chatter.py

--- src/chatter.py
+++ src/chatter.py
@@ -5,5 +5,5 @@ class DoubleA():
     def __init__(self):
         print("Bot initialized!")
         
-    def chat(this, query):
+    def chat(self, query):
         print(random.choice(keys.confused_responses))
