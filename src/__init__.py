e89f87d6f115f2a2a7f330b5dbfae9d7d81feb9e
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2023-03-02 11:25:17 -0500

    start on processing

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,14 @@
 import spacy
 
 nlp = spacy.load("en_core_web_sm")
+running = True
+raw = ""
+
+def run_ask():
+    global raw
+
+    raw = input('\nInput Command: ')
+    print(raw)
+
+while running:
+    run_ask()
