af232a8cadca259d238995422d959753808cb53d
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2023-03-02 14:09:56 -0500

    Update keys.py

--- src/keys.py
+++ src/keys.py
@@ -1,3 +1,3 @@
-exit_conditions = ("quit", "exit")
-confused_responses = ["I'm not sure how to respond.", "Can you try rewording that?"]
+exit_conditions = ("quit", "exit", "close")
+confused_responses = ["I'm not sure how to respond.", "Can you try rewording that?", "I do not have a programmed response for that.", "I do not understand."]
 project_condition = "open project "
