86f78addf8b4c15e1d4423ffbd542189ef66af4d
SlickFromMars <minecraftsummer2017@gmail.com>
2023-03-02 21:08:52 -0500

    Update keys.py

--- src/keys.py
+++ src/keys.py
@@ -1,5 +1,5 @@
 exit_conditions = ("quit", "exit", "close")
-confused_responses = ["I'm not sure how to respond.", "Can you try rewording that?", "I do not have a programmed response for that.", "I do not understand."]
+confused_responses = ["I'm not sure how to respond.", "Can you try rewording that?", "I do not have a programmed response for that.", "I do not understand.", "I am not trained for a response to that."]
 project_condition = "open project "
 name_condition = "My name is "
 greetings = ["Hello there", "Greetings", "Hello"]
