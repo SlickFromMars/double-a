f5e07242398271493dd94b66c236c34867f21c57
SlickFromMars <minecraftsummer2017@gmail.com>
2023-03-06 15:47:03 -0500

    Update INSTALL.bat

--- scripts/INSTALL.bat
+++ scripts/INSTALL.bat
@@ -1,10 +1,6 @@
 @echo off
 cd ../
 title Library Installation
-echo Setting up virtual environment...
-echo.
-python -m venv venv
-echo.
 echo Installing libraries...
 echo.
 pip install -r requirements.txt
