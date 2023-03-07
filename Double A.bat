0b1b94512b398b052d207e1688ecd6756f6cebac
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2023-03-07 10:39:30 -0500

    auto install

--- Double A.bat
+++ Double A.bat
@@ -1,5 +1,8 @@
 @echo off
 title Double A (Artificial Assistant)
+echo Getting libraries (could take a while)...
+pip install -r requirements.txt --q
+echo.
 cd src/
 echo Starting Double A...
 echo.
--- scripts/INSTALL.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd ../
-title Library Installation
-echo Installing libraries...
-echo.
-pip install -r requirements.txt
-echo.
-echo Installation complete.
-pause
