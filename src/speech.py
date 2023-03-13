ab7cdc6b0dff1c1e2a263210293fe20c517e8f21
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2023-03-13 13:01:56 -0400

    speech fix

--- src/speech.py
+++ src/speech.py
@@ -9,9 +9,9 @@ def setup():
     engine = tts.init()
 
 def say(txt:str):
+    print(txt)
     if client.data["speechEnabled"] == True:
         if engine.isBusy:
             engine.stop()
         engine.say(txt)
         engine.runAndWait()
-    print(txt)
