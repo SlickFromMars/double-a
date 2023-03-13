3957c97d83ec211a03050d88479637700cc5ef3b
SlickFromMars <82390475+SlickFromMars@users.noreply.github.com>
2023-03-13 13:14:19 -0400

    developer file

--- src/chatter.py
+++ src/chatter.py
@@ -1,12 +1,10 @@
-import os
 import random
 import time
 import webbrowser
 
-import requests
-
 import calculate
 import client
+import developer
 import keys
 from speech import say
 
@@ -48,17 +46,7 @@ class DoubleA:
             self.greet()
         
         elif trimmed_query == "Show code license":
-            r = requests.get("https://raw.githubusercontent.com/SlickFromMars/double-a/main/LICENSE.txt")
-            if r:
-                print("\n" + r.text)
-                f = open(keys.license_path, "w")
-                f.write(r.text)
-            elif os.path.isfile(keys.license_path):
-                say(str(r.status_code) + " Error getting online license. Showing local version.")
-                f = open(keys.license_path, "r")
-                print("\n" + f.read())
-            else:
-                say("Could not get license!")
+            developer.show_license()
 
         elif trimmed_query.startswith("My name is "):
             name = trimmed_query.replace("My name is ", "")
@@ -70,15 +58,7 @@ class DoubleA:
             say("Your name is " + client.data["name"] + ".")
 
         elif trimmed_query == "Open GitHub project":
-            project = input("Project name > ")
-            project = project.replace(" ", "-")
-            path = os.path.expanduser("~/Documents/GitHub/" + project)
-            if os.path.isdir(path):
-                say("Opening " + project + ".")
-                os.system("code " + path)
-                os.system("github " + path)
-            else:
-                say(path + " is not a directory!")
+            developer.git_project()
 
         elif trimmed_query == "Generate scatter plot":
             calculate.scatter()
---
+++ src/developer.py
@@ -0,0 +1,31 @@
+import os
+
+import requests
+
+import keys
+from speech import say
+
+
+def git_project():
+    project = input("Project name > ")
+    project = project.replace(" ", "-")
+    path = os.path.expanduser("~/Documents/GitHub/" + project)
+    if os.path.isdir(path):
+        say("Opening " + project + ".")
+        os.system("code " + path)
+        os.system("github " + path)
+    else:
+        say(path + " is not a directory!")
+
+def show_license():
+    r = requests.get("https://raw.githubusercontent.com/SlickFromMars/double-a/main/LICENSE.txt")
+    if r:
+        print("\n" + r.text)
+        f = open(keys.license_path, "w")
+        f.write(r.text)
+    elif os.path.isfile(keys.license_path):
+        say(str(r.status_code) + " Error getting online license. Showing local version.")
+        f = open(keys.license_path, "r")
+        print("\n" + f.read())
+    else:
+        say("Could not get license!")
